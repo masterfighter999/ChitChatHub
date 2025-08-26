eebb148a0166d053ccf4171ed8b4e4adf23545ee
Swayam Chatterjee <swayamchatterjee37@gmail.com>
2025-08-26 00:51:29 +0000

    (index):1  Failed to load resource: the server responded with a status

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,16 @@
-import {genkit} from 'genkit';
+
+import {genkit, Plugin} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {firebase} from '@genkit-ai/firebase';
+
+const plugins: Plugin[] = [googleAI()];
+// Use Firebase plugin on Vercel.
+// Requires GCLOUD_PROJECT environment variable to be set.
+if (process.env.VERCEL) {
+  plugins.push(firebase());
+}
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins,
   model: 'googleai/gemini-2.0-flash',
 });
